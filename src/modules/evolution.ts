46e31da0ce305e998eaaafc21aa4a2257cb8e4a2
zen0 <79776344+zen0dev@users.noreply.github.com>
2022-02-16 01:36:27 +0000

    build

--- src/modules/evolution.ts
+++ src/modules/evolution.ts
@@ -747,6 +747,8 @@ export async function connectRealms(app) {
 
   try {
     for (const realm of app.db.evolutionRealms) {
+      if (realm.key === 'ptr1' || realm.key === 'tournament1') continue
+      
       if (!games.evolution.realms[realm.key]) {
         games.evolution.realms[realm.key] = {}
         for (const key in Object.keys(realm)) {
